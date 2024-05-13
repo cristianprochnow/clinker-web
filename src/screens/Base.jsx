040219c40084a87cf2906daca8f34554d0ba5f1a
Cristian Prochnow <cristianprochnow@gmail.com>
2024-05-13 07:58:47 -0300

    feat: add base apage initial structure

--- src/screens/Base.jsx
+++ src/screens/Base.jsx
@@ -1,12 +1,30 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 import '../styles/screens/Base.css';
+import FeatherIcon from 'feather-icons-react';
 
 export function Base() {
   return (
-    <main>
-      <span>Conteúdo base da tela </span>
-      <Outlet />
+    <main id="base-screen">
+      <aside>
+        <h1>Clincker</h1>
+
+        <nav>
+          <Link to="/">
+            <FeatherIcon icon="activity"/> Dashboard
+          </Link>
+          <Link to="/links">
+            <FeatherIcon icon="link"/> URLs
+          </Link>
+          <Link to="/profile">
+            <FeatherIcon icon="user"/> Perfil
+          </Link>
+        </nav>
+      </aside>
+
+      <article>
+        <Outlet/>
+      </article>
     </main>
   );
 }
