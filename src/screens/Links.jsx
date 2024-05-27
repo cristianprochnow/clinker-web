7be4e8247418f3a85ff4386bf8d516bfda243c8a
Cristian Prochnow <cristianprochnow@gmail.com>
2024-05-27 13:01:11 -0300

    feat: add empty element for links list

--- src/screens/Links.jsx
+++ src/screens/Links.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import FeatherIcon from 'feather-icons-react';
+
 import '../styles/screens/Links.css';
 
 export function Links() {
+  const [links, setLinks] = useState([]);
+
   return (
     <div id="links-screen">
       <header>
@@ -9,36 +13,52 @@ export function Links() {
       </header>
 
       <section>
-        <div>
-          <div>
-            <header>
-              <span>
-                https://<span>dribbble</span>.com/shots
-              </span>
+        {
+          links.length > 0
+            ? links.map((link, linkIndex) => (
+              <div className="link-item" key={linkIndex}>
+                <div>
+                  <header>
+                    <span>
+                      https://<span>dribbble</span>.com/shots
+                    </span>
 
-              <aside>
-                <button type="button">
-                  <FeatherIcon icon="edit-3" />
-                </button>
+                    <aside>
+                      <button type="button">
+                        <FeatherIcon icon="edit-3"/>
+                      </button>
 
-                <button type="button">
-                  <FeatherIcon icon="send" />
-                </button>
-              </aside>
-            </header>
+                      <button type="button">
+                        <FeatherIcon icon="send"/>
+                      </button>
+                    </aside>
+                  </header>
 
-            <span>
-              https://dribbble.com/shots/20422303-Shortie-URL-Shortener-Hero-Section
-            </span>
-          </div>
+                  <span>
+                    https://dribbble.com/shots/20422303-Shortie-URL-Shortener-Hero-Section
+                  </span>
+                </div>
 
-          <footer>
-            <FeatherIcon icon="clock"/>
-            <span>
-              02 de Dezembro de 2024
-            </span>
-          </footer>
-        </div>
+                <footer>
+                  <FeatherIcon icon="clock"/>
+                  <span>
+                    02 de Dezembro de 2024
+                  </span>
+                </footer>
+              </div>
+            ))
+            : (
+              <div className="empty-item">
+                <div>
+                  <img src="/empty.svg" />
+                </div>
+
+                <p>
+                  Nenhum link encontrado. Encurte alguma URL para começar.
+                </p>
+              </div>
+            )
+        }
       </section>
     </div>
   );
