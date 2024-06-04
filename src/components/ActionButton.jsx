09a4f4d59aa1ea4ac95ed6673ecb646da6ddca09
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-03 23:13:35 -0300

    feat: add props of click and disabled to action button

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,7 +1,9 @@
 import '../styles/components/ActionButton.css';
 
-export function ActionButton({ type = 'button', children }) {
+export function ActionButton({ type = 'button', onClick, disabled, children }) {
   return (
-    <button className="action-button" type={type}>{children}</button>
+    <button className="action-button" type={type} onClick={onClick} disabled={disabled}>
+      {children}
+    </button>
   );
 }
