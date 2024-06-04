9d2fa9ad7d4d6678f4fb86ca5c2a6a616c3f3686
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-03 23:14:05 -0300

    feat: add structure of profile screen

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,11 +1,68 @@
+import FeatherIcon from 'feather-icons-react';
+
+import { PageTitle } from '../components/PageTitle.jsx';
+
 import '../styles/screens/Profile.css';
+import { ActionButton } from '../components/ActionButton.jsx';
+import { FormInput } from '../components/FormInput.jsx';
+import { useState } from 'react';
 
 export function Profile() {
+  const [isEditing, setEditing] = useState(false);
+
+  function onEditDataHandler() {
+    setEditing(!isEditing);
+  }
+
+  function onSaveDataHandler() {
+    setEditing(false);
+  }
+
   return (
     <div id="profile-screen">
+      <PageTitle>Perfil</PageTitle>
+
       <header>
-        <h1><span>P</span>erfil</h1>
+        <section>
+          <img src="/profile-thumb.jpg" alt="Connected World Wide Web" />
+
+          <span>
+            <a href="https://unsplash.com/photos/aerial-photography-of-city-during-night-time-1lfI7wkGWZ4">
+              <FeatherIcon icon="external-link"/>
+            </a>
+          </span>
+
+          <div>
+            <span>
+              <FeatherIcon icon="user" size={48}/>
+            </span>
+          </div>
+        </section>
+
+        <footer>
+          <span>contato@contato.com.br</span>
+          <span>Usuário da Aplicação</span>
+          <span></span>
+          <span>
+            <FeatherIcon icon="calendar" size={16}/>
+            Entrou em 23/05/2023
+          </span>
+        </footer>
       </header>
+
+      <footer>
+        <ActionButton onClick={onEditDataHandler}>Editar Dados</ActionButton>
+
+        <div>
+          <div>
+            <FormInput label="Nome" value="Usuário da Aplicação" disabled={!isEditing}/>
+            <FormInput type="password" label="Senha" value="NOT_PASSWORD" disabled={!isEditing}/>
+            <FormInput type="password" label="Confirmar Senha" value="NOT_PASSWORD" disabled={!isEditing}/>
+          </div>
+
+          <ActionButton onClick={onSaveDataHandler} disabled={!isEditing}>Salvar</ActionButton>
+        </div>
+      </footer>
     </div>
   );
 }
