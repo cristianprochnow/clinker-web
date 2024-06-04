3d83b0870657efef22f2ef171ba5d44a3a01cdcf
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-03 21:55:14 -0300

    feat: add setup for profile page

---
+++ src/screens/Profile.jsx
@@ -0,0 +1,11 @@
+import '../styles/screens/Profile.css';
+
+export function Profile() {
+  return (
+    <div id="profile-screen">
+      <header>
+        <h1><span>P</span>erfil</h1>
+      </header>
+    </div>
+  );
+}
