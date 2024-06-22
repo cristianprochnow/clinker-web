5c3324d980a1c11d6020aa1e29f2a613531fb0df
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-22 16:43:20 -0300

    feat: add auth headers helper

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 export const AUTH_SESSION_KEY_ID = 'clincker@auth:id';
 export const AUTH_SESSION_KEY_TOKEN = 'clincker@auth:token';
+export const AUTH_HEADER_USER = 'CLINCKER-USER';
+export const AUTH_HEADER_TOKEN = 'CLINCKER-TOKEN';
