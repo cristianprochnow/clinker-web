69cb0e603ac617ad1560b32f7e7065f32256dc91
Cristian Prochnow <cristianprochnow@gmail.com>
2024-06-24 00:20:10 -0300

    feat: use separated date function in profile

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 
 import { Http } from '../api/http.js';
 import { useAuth } from '../contexts/auth.jsx';
+import { formatDate } from '../utils/date.js';
 import { NO_PASS_VALUE } from '../utils/constants.js';
 import { PageTitle } from '../components/PageTitle.jsx';
 import { ActionButton } from '../components/ActionButton.jsx';
@@ -59,16 +60,6 @@ export function Profile() {
   useEffect(() => {
     loadProfile();
   }, []);
-
-  function formatDate(date) {
-    const formatDate = new Date(date);
-
-    const day = formatDate.getDate().toString().padStart(2, '0');
-    const month = (formatDate.getMonth() + 1).toString().padStart(2, '0');
-    const year = formatDate.getFullYear();
-
-    return `${day}/${month}/${year}`;
-  }
 
   function onEditDataHandler() {
     setEditing(!isEditing);
